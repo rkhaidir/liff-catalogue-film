2ca01ef5b11ffb12596e73caa0b64c5d32beef65
Rakhmat Khaidir <rkhaidir085@gmail.com>
2020-02-18 20:47:12 +0800

    initial commit 2

--- liff-starter.js
+++ liff-starter.js
@@ -2,7 +2,7 @@ var userName = 'Guest';
 
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1653721993-E9RBl7Rz";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1653721321-DveO49Xb";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
